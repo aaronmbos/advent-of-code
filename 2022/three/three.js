08f135ed594e44ca074d349184fa23b03ab286dc
Aaron Bos <aaronmbos@gmail.com>
2022-12-06 20:59:12 -0600

    Finish part two

--- 2022/three/three.js
+++ 2022/three/three.js
@@ -18,10 +18,45 @@ const isUppercase = (char) => char.charCodeAt(0) < 97;
 const calculateValue = (char) =>
   isUppercase(char) ? char.charCodeAt(0) - 38 : char.charCodeAt(0) - 96;
 
-let totalValue = 0;
+function partTwo() {
+  const file = getFile();
+
+  let totalValue = 0;
+  let group = [];
+  file.on("line", (line) => {
+    if (group.length < 3) {
+      group.push(line.trim());
+    }
+
+    if (group.length === 3) {
+      let isMatch = false;
+      for (const char of group[0]) {
+        for (const char2 of group[1]) {
+          for (const char3 of group[2]) {
+            if (char === char2 && char === char3) {
+              totalValue += calculateValue(char);
+              isMatch = true;
+              break;
+            }
+          }
+          if (isMatch) {
+            break;
+          }
+        }
+        if (isMatch) {
+          break;
+        }
+      }
+      console.log(totalValue);
+      group = [];
+    }
+  });
+}
 
 function partOne() {
   const file = getFile();
+
+  let totalValue = 0;
 
   file.on("line", (line) => {
     const midpoint = line.trim().length / 2;
@@ -35,8 +70,6 @@ function partOne() {
         if (char === char2) {
           totalValue += calculateValue(char);
           isMatch = true;
-        }
-        if (isMatch) {
           break;
         }
       }
@@ -49,4 +82,4 @@ function partOne() {
   });
 }
 
-partOne();
+partTwo();
