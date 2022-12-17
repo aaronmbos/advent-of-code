ebeecf07ce5f26bb0dee6876a1e063c0cbc71fbc
Aaron Bos <aaronmbos@gmail.com>
2022-12-16 19:22:16 -0600

    Finally finish day 9 part 1

--- 2022/nine/nine.mjs
+++ 2022/nine/nine.mjs
@@ -31,64 +31,37 @@ function handleAction(direction, count, head, tail) {
     moveTail(head, tail);
   }
 }
-
+let tailPlaceCount = 0;
 function moveTail(head, tail) {
-  let xDelta = head.current[0] - tail.current[0];
-  let yDelta = head.current[1] - tail.current[1];
+  const xDelta = head.current[0] - tail.current[0];
+  const yDelta = head.current[1] - tail.current[1];
 
-  if (xDelta > 0 && yDelta < 0) {
-    // down and to right
-    if (xDelta > 1) {
-      xDelta -= 1;
+  if (xDelta !== 0 && yDelta !== 0) {
+    if (xDelta > 0 && yDelta > 0) {
+      tail.current = [tail.current[0] + 1, tail.current[1] + 1];
+    } else if (xDelta > 0 && yDelta < 0) {
+      tail.current = [tail.current[0] + 1, tail.current[1] - 1];
+    } else if (xDelta < 0 && yDelta < 0) {
+      tail.current = [tail.current[0] - 1, tail.current[1] - 1];
+    } else if (xDelta < 0 && yDelta > 0) {
+      tail.current = [tail.current[0] - 1, tail.current[1] + 1];
     }
-    if (yDelta < -1) {
-      yDelta += 1;
-    }
-  } else if (xDelta < 0 && yDelta > 0) {
-    // up and to left
-    if (xDelta < -1) {
-      xDelta += 1;
-    }
-    if (yDelta > 1) {
-      yDelta -= 1;
-    }
-  } else if (yDelta < 0 && xDelta < 0) {
-    // down and to left
-    if (xDelta < -1) {
-      xDelta += 1;
-    }
-    if (yDelta < -1) {
-      yDelta += 1;
-    }
-  } else if (yDelta > 0 && xDelta > 0) {
-    // up and to right
-    if (xDelta > 1) {
-      xDelta -= 1;
-    }
-    if (yDelta > 1) {
-      yDelta -= 1;
-    }
-  } else if (yDelta > 0 && xDelta === 0) {
-    // up
-    yDelta -= 1;
-  } else if (yDelta < 0 && xDelta === 0) {
-    // down
-    yDelta += 1;
-  } else if (xDelta > 0 && yDelta === 0) {
-    // right
-    xDelta -= 1;
-  } else if (xDelta < 0 && yDelta === 0) {
-    // left
-    xDelta += 1;
+  } else if (xDelta !== 0) {
+    tail.current =
+      xDelta > 0
+        ? [tail.current[0] + 1, tail.current[1]] // right
+        : [tail.current[0] - 1, tail.current[1]]; // left
+  } else if (yDelta !== 0) {
+    tail.current =
+      yDelta > 0
+        ? [tail.current[0], tail.current[1] + 1] // up
+        : [tail.current[0], tail.current[1] - 1]; // down
   } else {
-    console.log("huh", xDelta, yDelta);
-    throw new Error("Unexpected movement");
+    throw new Error("Unknown tail movement");
   }
-
-  tail.current = [tail.current[0] + xDelta, tail.current[1] + yDelta];
   if (
     !tail.history.find((point) => {
-      return point[0] === tail.current[0] && point[1] === tail.current[1];
+      return tail.current[0] === point[0] && tail.current[1] === point[1];
     })
   ) {
     tail.history.push(tail.current);
@@ -116,13 +89,14 @@ function moveHead(direction, head) {
 }
 
 function areTouching(head, tail) {
-  if (
-    Math.abs(head.current[0] - tail.current[0]) <= 1 &&
-    Math.abs(head.current[1] - tail.current[1] <= 1)
-  ) {
-    return true;
+  if (Math.abs(head.current[0] - tail.current[0]) > 1) {
+    return false;
   }
-  return false;
+
+  if (Math.abs(head.current[1] - tail.current[1]) > 1) {
+    return false;
+  }
+  return true;
 }
 
 async function partOne() {
@@ -141,8 +115,6 @@ async function partOne() {
   for (const action of actions) {
     handleAction(action.direction, action.count, head, tail);
   }
-  console.log(tail);
-  console.log(head);
   console.log(tail.history.length);
 }
 
