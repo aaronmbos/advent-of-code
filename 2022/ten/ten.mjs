c263345e0111f99e9c644fef7e1425b3ed785ffe
Aaron Bos <aaronmbos@gmail.com>
2022-12-18 20:30:45 -0600

    Finish day 10 part 2

--- 2022/ten/ten.mjs
+++ 2022/ten/ten.mjs
@@ -20,6 +20,54 @@ function parseSignal(rawSignal) {
   }
 }
 
+function partTwo(lines) {
+  const signals = lines.map((line) => parseSignal(line));
+
+  let cycleCount = 0;
+  let crtIdx = 0;
+  let spriteCenterIdx = 1;
+  let crt = [[]];
+  let crtRow = 0;
+  let register = 1;
+
+  for (const signal of signals) {
+    for (let idx = 0; idx < signal.cycles; idx++) {
+      cycleCount++;
+      if (isOverlappingSprite(crtIdx, spriteCenterIdx)) {
+        crt[crtRow][crtIdx] = "#";
+      } else {
+        crt[crtRow][crtIdx] = ".";
+      }
+      crtIdx++;
+      if (crtIdx === 40) {
+        crtIdx = 0;
+        crtRow++;
+        crt.push([]);
+      }
+    }
+    spriteCenterIdx += signal.value;
+    register += signal.value;
+  }
+
+  printCrt(crt);
+}
+
+function printCrt(crt) {
+  let display = "";
+  for (const row of crt) {
+    display += row.join("");
+    display += "\n";
+  }
+  console.log(display);
+}
+
+function isOverlappingSprite(crtPosition, spriteCenterIndex) {
+  // The sprite is 3 pixels wide, but only tracking the center of it
+  return (
+    crtPosition >= spriteCenterIndex - 1 && crtPosition <= spriteCenterIndex + 1
+  );
+}
+
 function partOne(lines) {
   const signals = lines.map((line) => parseSignal(line));
 
@@ -34,13 +82,13 @@ function partOne(lines) {
       if (cycleCount <= 220 && cycleCount === cycleValue) {
         strengthSum += cycleCount * register;
         cycleValue += 40;
-        console.log("cycles", cycleCount);
-        console.log("register", register);
-        console.log("strength", strengthSum);
       }
     }
     register += signal.value;
   }
+
+  console.log("strength", strengthSum);
 }
 
 partOne(await getFileLines());
+partTwo(await getFileLines());
